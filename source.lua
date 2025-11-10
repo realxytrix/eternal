ee5de5e6cfba39ef155d430f23a385fee9df4c20
kitodoescode <156444563+kitodoescode@users.noreply.github.com>
2025-11-11 01:41:29 +0530

    Update source.lua

--- source.lua
+++ source.lua
@@ -14,6 +14,34 @@ local esp_objects = {}
 local date = os.date("%d-%m-%y")
 local last
 
+local lp = players.LocalPlayer
+local lp_char = lp.Character
+
+local bone_connections_r6 = {
+	{ "Head", "Torso" },
+	{ "Torso", "Right Arm" },
+	{ "Torso", "Left Arm" },
+	{ "Torso", "Right Leg" },
+	{ "Torso", "Left Leg" },
+}
+
+local bone_connections_r15 = {
+	{ "Head", "UpperTorso" },
+	{ "UpperTorso", "LowerTorso" },
+	{ "LowerTorso", "LeftUpperLeg" },
+	{ "LowerTorso", "RightUpperLeg" },
+	{ "LeftUpperLeg", "LeftLowerLeg" },
+	{ "LeftLowerLeg", "LeftFoot" },
+	{ "RightUpperLeg", "RightLowerLeg" },
+	{ "RightLowerLeg", "RightFoot" },
+	{ "UpperTorso", "LeftUpperArm" },
+	{ "UpperTorso", "RightUpperArm" },
+	{ "LeftUpperArm", "LeftLowerArm" },
+	{ "LeftLowerArm", "LeftHand" },
+	{ "RightUpperArm", "RightLowerArm" },
+	{ "RightLowerArm", "RightHand" },
+}
+
 local window = ui:Window({Name="eternal | @kitodoescode"}) do
 	local watermark = window:Watermark({
 		Title="eternal | @kitodoescode | " .. date,
@@ -51,68 +79,102 @@ end
 local function add_player_esp(player)
     if esp_objects[player] then return end
 
-    local box = draw("Square", 
-	    {
-			Visible = false,
-			Color = Color3.new(1, 1, 1),
-			Size = Vector2.new(0, 0),
-			Thickness = 1,
-			ZIndex = 2,
-		}
-	)
+    local box = draw("Square", {
+		Visible = false,
+		Color = Color3.new(1, 1, 1),
+		Size = Vector2.new(0, 0),
+		Thickness = 1,
+		ZIndex = 20,
+	})
 
-	local box_outline = draw("Square", 
-	    {
-			Visible = false,
-			Color = Color3.new(0, 0, 0),
-			Size = Vector2.new(0, 0),
-			Thickness = 3,
-		}
-	)
+	local box_outline = draw("Square", {
+		Visible = false,
+		Color = Color3.new(0, 0, 0),
+		Size = Vector2.new(0, 0),
+		Thickness = 3,
+	})
 
-	local name = draw("Text",
-		{
-			Visible = false,
-			Color = Color3.new(1, 1, 1),
-			Text = player.Name,
-			Center = true,
-			Outline = true,
-			OutlineColor = Color3.new(0, 0, 0),
-		}
-	)
+	local name = draw("Text",{
+		Visible = false,
+		Color = Color3.new(1, 1, 1),
+		Text = player.Name,
+		Center = true,
+		Outline = true,
+		OutlineColor = Color3.new(0, 0, 0),
+	})
 
-	local distance = draw("Text",
-		{
-			Visible = false,
-			Color = Color3.new(1, 1, 1),
-			Text = "[ 0.0m ]",
-			Center = true,
-			Outline = true,
-			OutlineColor = Color3.new(0, 0, 0),
-		}
-	)
+	local distance = draw("Text",{
+		Visible = false,
+		Color = Color3.new(1, 1, 1),
+		Text = "[ 0.0m ]",
+		Center = true,
+		Outline = true,
+		OutlineColor = Color3.new(0, 0, 0),
+	})
+	
+	local skeleton = { lines = {}, bone_set = nil, rig_detected = false }
 
-	local objects = { box = box, box_outline = box_outline, name = name, distance = distance }
+	local objects = { box = box, box_outline = box_outline, name = name, distance = distance, skeleton = skeleton }
+
+	local function hide_esp_objects()
+		objects.box.Visible = false
+		objects.box_outline.Visible = false
+		objects.name.Visible = false
+		objects.distance.Visible = false
+		for _, bone_table in ipairs(objects.skeleton.lines) do
+			bone_table.joint.Visible = false
+			bone_table.joint_outline.Visible = false
+		end
+	end
 
 	local connection = runservice.RenderStepped:Connect(function()
 	    local char = player.Character
-		if not char or not char:FindFirstChild("HumanoidRootPart") or not char:FindFirstChild("Head") then
-			objects.box.Visible = false
-			objects.box_outline.Visible = false
-			objects.name.Visible = false
-			objects.distance.Visible = false
-			return
-		end
+    	if not char then 
+        	hide_esp_objects()
+        	return 
+    	end
+    
+    	local hrp = char:FindFirstChild("HumanoidRootPart")
+    	local head = char:FindFirstChild("Head")
 
-		local hrp = char:FindFirstChild("HumanoidRootPart")
-		local head = char:FindFirstChild("Head")
+    	if not hrp or not head then
+    	    hide_esp_objects()
+    	    return
+    	end
+
+		if not objects.skeleton.rig_detected then
+			local humanoid = char:FindFirstChild("Humanoid")
+            if humanoid.RigType == Enum.HumanoidRigType.R6 then
+				objects.skeleton.bone_set = bone_connections_r6
+			else
+				objects.skeleton.bone_set = bone_connections_r15
+			end
+
+            objects.skeleton.rig_detected = true
+
+			for idx, bone_joint in pairs(objects.skeleton.bone_set) do
+				local from_bone, to_bone = bone_joint[1], bone_joint[2]
+
+				local joint = draw("Line", {
+					Visible = false,
+					Color = Color3.new(1, 1, 1),
+					Thickness = 1,
+					ZIndex = 2,
+				})
+		
+				local joint_outline = draw("Line", {
+					Visible = false,
+					Color = Color3.new(0, 0, 0),
+					Thickness = 3,
+				})
+
+				objects.skeleton.lines[idx] = { joint = joint, joint_outline = joint_outline, from_bone = from_bone, to_bone = to_bone }
+			end
+		end
 
 		local _, visible = world_to_screen(hrp.Position)
 		if not visible then
-			objects.box.Visible = false
-			objects.box_outline.Visible = false
-			objects.name.Visible = false
-			objects.distance.Visible = false
+			hide_esp_objects()
 			return
 		end
 
@@ -138,14 +200,42 @@ local function add_player_esp(player)
 
 		local bottom_center = Vector2.new(head_pos.x, top_left.y + h + 10)
 		local our_pos
-		if not players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+		if not lp_char:FindFirstChild("HumanoidRootPart") then
 			our_pos = workspace.CurrentCamera.CFrame.Position
 		else
-			our_pos = players.LocalPlayer.Character.HumanoidRootPart.Position
+			our_pos = lp_char.HumanoidRootPart.Position
 		end
 		objects.distance.Text = string.format("[ %.1fm ]", (our_pos - hrp.Position).Magnitude)
 		objects.distance.Position = bottom_center
 		objects.distance.Visible = true
+
+		if objects.skeleton.rig_detected then
+			for _, bone_table in ipairs(objects.skeleton.lines) do
+				local from_bone, to_bone = char:FindFirstChild(bone_table.from_bone), char:FindFirstChild(bone_table.to_bone)
+				if not from_bone or not to_bone then
+					bone_table.joint.Visible = false
+					bone_table.joint_outline.Visible = false
+					return
+				end
+
+				local from_pos, from_visibility = world_to_screen(from_bone.Position)
+				local to_pos, to_visibility = world_to_screen(to_bone.Position)
+
+				if not from_visibility or not to_visibility then
+					bone_table.joint.Visible = false
+					bone_table.joint_outline.Visible = false
+					return
+				end
+
+				bone_table.joint.From = Vector2.new(from_pos.x, from_pos.y)
+				bone_table.joint.To = Vector2.new(to_pos.x, to_pos.y)
+				bone_table.joint.Visible = true
+
+				bone_table.joint_outline.From = Vector2.new(from_pos.x, from_pos.y)
+				bone_table.joint_outline.To = Vector2.new(to_pos.x, to_pos.y)
+				bone_table.joint_outline.Visible = true
+			end
+		end
 	end)
 
 	local connections = { connection }
@@ -155,20 +245,27 @@ end
 
 -- function to remove esp objects and connections from player
 local function remove_player_esp(player)
-    local idk = esp_objects[player]
-	if not idk then return end
+    local player_table = esp_objects[player]
+	if not player_table then return end
 
-    for _, con in pairs(idk.connections) do
-		if con.Connected then
-			con:Disconnect()
+    for _, connection in pairs(player_table.connections) do
+		if connection.Connected then
+			connection:Disconnect()
 		end
 	end
 
-    for _, obj in pairs(idk.objects) do
-		obj:Destroy()
-	end
+    for name, draw_object in pairs(player_table.objects) do
+        if name == "skeleton" then
+            for _, b in ipairs(player_table.objects.skeleton.lines) do
+                b.joint:Destroy()
+                b.joint_outline:Destroy()
+            end
+        else
+            draw_object:Destroy()
+        end
+    end
 
-	idk = nil
+	player_table = nil
 end
 
 -- adding esp objects and connections on all current player
@@ -178,8 +275,9 @@ end
 
 -- add esp objects and connections on any new player that joined
 players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Wait()
-    add_player_esp(player)
+    player.CharacterAdded:Connect(function()
+        add_player_esp(player)
+    end)
 end)
 
 -- remove esp objects and connections from any new player that left
